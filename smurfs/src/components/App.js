fa2574e6b092959aa64ab125e588f9b67a034f98
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-09 11:15:17 -0500

    added another action and added it to the reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+import Smurfs from "./Smurfs";
+
 import "./App.css";
+
 class App extends Component {
+  getSmurfs = e => {
+    e.preventDefault();
+    this.props.getSmurfs();
+  };
   render() {
     return (
       <div className="App">
@@ -8,9 +17,11 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <button onClick={this.getSmurfs}>Get Smurfs!</button>
+        <Smurfs />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, { getSmurfs })(App);
